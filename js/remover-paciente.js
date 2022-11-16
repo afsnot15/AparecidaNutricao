2b856ba523eb38dee2c9c93639df71cf3c1956d8
Afonso Gomes <afsnot15@gmail.com>
2022-11-15 22:49:48 -0300

    Delegando eventos

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,14 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(paciente => {
-    paciente.addEventListener("dblclick", function () {
-        console.log("Fui clicado com duplo click");
-        this.remove();
-    });
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick", function (event) {
+    event.target.parentNode.remove();
 });
+
+//pacientes.forEach(paciente => {
+ //   paciente.addEventListener("dblclick", function () {
+   //     console.log("Fui clicado com duplo click");
+     //   this.remove();
+    //});
+//});
